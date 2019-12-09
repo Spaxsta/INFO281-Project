7e5abc9b5b4811f2455e79fa336048849ca145da
Devon Kennedy <kenneddevo@myvuw.ac.nz>
2019-12-09 16:04:10 +1300

    Added nz map and school locations

--- app.R
+++ app.R
@@ -1,69 +1,38 @@
 library(shiny)
 library(leaflet)
 library(RColorBrewer)
+library(tidyverse)
 
-ui <- bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-                  sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                              value = range(quakes$mag), step = 0.1
-                  ),
-                  selectInput("colors", "Color Scheme",
-                              rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                  ),
-                  checkboxInput("legend", "Show legend", TRUE)
-    )
+wvs <- read.csv("LocationData.csv")
+
+Lat <- wvs$Latitude
+
+Lon <- wvs$Longitude
+
+schoolNames <- wvs$Org.Name
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+    leafletOutput("mymap"),
+    p()
 )
 
 server <- function(input, output, session) {
     
-    # Reactive expression for the data subsetted to what the user selected
-    filteredData <- reactive({
-        quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
-    })
-    
-    # This reactive expression represents the palette function,
-    # which changes as the user makes selections in UI.
-    colorpal <- reactive({
-        colorNumeric(input$colors, quakes$mag)
-    })
-    
-    output$map <- renderLeaflet({
-        # Use leaflet() here, and only include aspects of the map that
-        # won't need to change dynamically (at least, not unless the
-        # entire map is being torn down and recreated).
-        leaflet(quakes) %>% addTiles() %>%
-            fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
-    })
-    
-    # Incremental changes to the map (in this case, replacing the
-    # circles when a new color is chosen) should be performed in
-    # an observer. Each independent set of things that can change
-    # should be managed in its own observer.
-    observe({
-        pal <- colorpal()
-        
-        leafletProxy("map", data = filteredData()) %>%
-            clearShapes() %>%
-            addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
-                       fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
-            )
-    })
-    
-    # Use a separate observer to recreate the legend as needed.
-    observe({
-        proxy <- leafletProxy("map", data = quakes)
-        
-        # Remove any existing legend, and only if the legend is
-        # enabled, create a new one.
-        proxy %>% clearControls()
-        if (input$legend) {
-            pal <- colorpal()
-            proxy %>% addLegend(position = "bottomright",
-                                pal = pal, values = ~mag
-            )
-        }
+    points <- eventReactive(input$recalc, {
+        cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+    }, ignoreNULL = FALSE)
+
+    output$mymap <- renderLeaflet({
+        leaflet() %>%
+            setView(lng = 175, lat = -41, zoom = 7) %>%
+            addProviderTiles(providers$Stamen.TonerLite,
+                             options = providerTileOptions(noWrap = TRUE)
+            ) %>%
+            addCircleMarkers(data = wvs, lng = ~Lon, lat = ~Lat, popup = ~schoolNames, stroke = FALSE, fillOpacity = 0.5, label =  ~schoolNames, 
+                             labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T))
     })
 }
 
